02190d8134a7f3ce610c5e65379c6c707982d270
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-10-17 10:53:20 +1100

    bug fixing delete note function

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -24,37 +24,3 @@ router.delete("/notes/:id", (req, res) => {
 });
 
 module.exports = router;
-
-// router.get("/api/notes", (req, res) => {
-//   fs.readFile("./db/db.json", (err, data) => {
-//     if (err) throw err;
-//     console.log(JSON.parse(data));
-
-//     res.send(data);
-//   });
-// });
-
-// router.post("/api/notes", (req, res) => {
-//   let newNote = {
-//     id: uuid(),
-//     title: req.body.title,
-//     text: req.body.text,
-//   };
-
-//   fs.readFile("./db/db.json", (err, data) => {
-//     if (err) throw err;
-
-//     let newData = JSON.parse(data);
-
-//     newData.push(newNote);
-//     console.log(newData);
-
-//     fs.writeFile("./db/db.json", JSON.stringify(newData), (err) => {
-//       if (err) throw err;
-
-//       res.send("successfully added");
-//     });
-//   });
-// });
-
-// module.exports = router;
--- routes/notes.js
+++ routes/notes.js
@@ -2,23 +2,7 @@ const fs = require("fs");
 
 const path = require("path");
 
-function noteCreateNewNote(body, noteTakerArray) {
-  const note = body;
-  noteTakerArray.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, "../db/db.json"),
-    JSON.stringify(
-      {
-        notes: noteTakerArray,
-      },
-      null,
-      2
-    )
-  );
-  return note;
-}
-
-function noteDeleteNote(noteTakerArray, id) {
+function deleteNote(noteTakerArray, id) {
   let deleteID = parseInt(id);
   noteTakerArray.splice(deleteID, 1);
 
@@ -38,7 +22,23 @@ function noteDeleteNote(noteTakerArray, id) {
   );
 }
 
+function createNote(body, noteTakerArray) {
+  const note = body;
+  noteTakerArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, "../db/db.json"),
+    JSON.stringify(
+      {
+        notes: noteTakerArray,
+      },
+      null,
+      2
+    )
+  );
+  return note;
+}
+
 module.exports = {
-  noteCreateNewNote,
-  noteDeleteNote,
+  createNote,
+  deleteNote,
 };
