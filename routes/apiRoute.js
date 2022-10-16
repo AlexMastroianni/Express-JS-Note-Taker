b8f973244e4e6f7e54ff2c163fecc10a91b38c97
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-10-17 10:47:32 +1100

    bug fixes and housekeeping

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -3,36 +3,58 @@ const db = require("../db/db.json");
 const router = require("express").Router();
 const uuid = require("uuid");
 
-router.get("/api/notes", (req, res) => {
-  fs.readFile("./db/db.json", (err, data) => {
-    if (err) throw err;
-    console.log(JSON.parse(data));
+const { notes } = require("../db/db");
 
-    res.send(data);
-  });
+const { noteCreateNewNote, noteDeleteNote } = require("../routes/notes");
+
+router.get("/notes", (req, res) => {
+  let saved = notes;
+  res.json(saved);
 });
 
-router.post("/api/notes", (req, res) => {
-  let newNote = {
-    id: uuid(),
-    title: req.body.title,
-    text: req.body.text,
-  };
+router.post("/notes", (req, res) => {
+  req.body.id = notes.length.toString();
+  let note = noteCreateNewNote(req.body, notes);
+  res.json(note);
+});
 
-  fs.readFile("./db/db.json", (err, data) => {
-    if (err) throw err;
-
-    let newData = JSON.parse(data);
-
-    newData.push(newNote);
-    console.log(newData);
-
-    fs.writeFile("./db/db.json", JSON.stringify(newData), (err) => {
-      if (err) throw err;
-
-      res.send("successfully added");
-    });
-  });
+router.delete("/notes/:id", (req, res) => {
+  noteDeleteNote(notes, req.params.id);
+  res.json(notes);
 });
 
 module.exports = router;
+
+// router.get("/api/notes", (req, res) => {
+//   fs.readFile("./db/db.json", (err, data) => {
+//     if (err) throw err;
+//     console.log(JSON.parse(data));
+
+//     res.send(data);
+//   });
+// });
+
+// router.post("/api/notes", (req, res) => {
+//   let newNote = {
+//     id: uuid(),
+//     title: req.body.title,
+//     text: req.body.text,
+//   };
+
+//   fs.readFile("./db/db.json", (err, data) => {
+//     if (err) throw err;
+
+//     let newData = JSON.parse(data);
+
+//     newData.push(newNote);
+//     console.log(newData);
+
+//     fs.writeFile("./db/db.json", JSON.stringify(newData), (err) => {
+//       if (err) throw err;
+
+//       res.send("successfully added");
+//     });
+//   });
+// });
+
+// module.exports = router;
--- server.js
+++ server.js
@@ -11,23 +11,9 @@ app.use(express.json());
 app.use("/api", apiRoute);
 app.use("/", htmlRoute);
 
-// Creates listener which starts the server
 app.listen(PORT, function () {
   console.log(`App is listening on Port ${PORT}`);
 });
-
-// app.use("/", function (req, res) {
-//   res.sendFile(path.join(__dirname, "public/index.html"));
-// });
-
-// // GET route sending user to NOTES page
-// app.get("/notes", function (req, res) {
-//   res.sendFile(path.join(__dirname, "public/notes.html"));
-// });
-
-// app.get("/api/notes", function (req, res) {
-//   res.sendFile(path.join(__dirname, "db/db.json"));
-// });
 
 // app.post("/api/notes", function (req, res) {
 //   fs.readFile(
@@ -58,23 +44,23 @@ app.listen(PORT, function () {
 //   );
 // });
 
-app.delete("/api/notes/:id", function (req, res) {
-  const deleteID = req.params.id;
-  fs.readFile("db.json", "utf8", function (error, response) {
-    if (error) {
-      console.log(error);
-    }
-    let notes = JSON.parse(response);
-    if (deleteID <= notes.length) {
-      res.json(notes.splice(deleteID - 1, 1));
-      for (let i = 0; i < notes.length; i++) {
-        notes[i].id = i + 1;
-      }
-      fs.writeFile("db.json", JSON.stringify(notes, null, 2), function (err) {
-        if (err) throw err;
-      });
-    } else {
-      res.json(false);
-    }
-  });
-});
+// app.delete("/api/notes/:id", function (req, res) {
+//   const deleteID = req.params.id;
+//   fs.readFile("db.json", "utf8", function (error, response) {
+//     if (error) {
+//       console.log(error);
+//     }
+//     let notes = JSON.parse(response);
+//     if (deleteID <= notes.length) {
+//       res.json(notes.splice(deleteID - 1, 1));
+//       for (let i = 0; i < notes.length; i++) {
+//         notes[i].id = i + 1;
+//       }
+//       fs.writeFile("db.json", JSON.stringify(notes, null, 2), function (err) {
+//         if (err) throw err;
+//       });
+//     } else {
+//       res.json(false);
+//     }
+//   });
+// });
