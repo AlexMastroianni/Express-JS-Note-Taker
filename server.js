0831c7bbf6f857b56f4938d0dd9986e05713bf6d
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-10-16 14:57:56 +1100

    fixing bug will pulling from db

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-// Creates Express App
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
